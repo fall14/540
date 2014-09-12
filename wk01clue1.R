4b1c2687529e7a5e1581945795e5f1cd62fa0b72
manaswini248 <manaswini.s.thakur@gmail.com>
2014-09-11 20:18:41 -0400

    my weekely project using clues

--- wk01clue1.R
+++
@@ -1,8 +0,0 @@
-dayCountry <- "Liberia"
-casesCountry <- "Nigeria"
-
-days  <- eval(parse(text=paste("ebola$Day", dayCountry, sep="_")))
-cases <- eval(parse(text=paste("ebola$Cases", casesCountry, sep="_")))
-
-print( days )
-print( cases )
--- wk01clue3.R
+++
@@ -1,7 +0,0 @@
-numDays <- length(days)
-
-mostRecentDate <- ebola$Date[1]
-oldestDate <- ebola$Date[numDays]
-
-print( mostRecentDate )
-print( oldestDate )
